553df1748716252964498bafea0a421c6fba2274
Ekaterina Boldareva <k.boldareva96@yandex.ru>
2019-10-07 12:49:23 +0500

    Запуск приложения 2

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,10 @@ const questionsUrl= 'http://proto.io/en/jobs/candidate-questions/quiz.json';
 const resultUrl= 'http://proto.io/en/jobs/candidate-questions/result.json';
 
 const load = (url) => window.fetch(url)
-    .then(response => {
-        console.log(response);
-        return response.json();
+    .then(response => response.json())
+    .catch(error => {
+        console.log('Что-то пошло совсем не так...');
+        console.log('Error: ', error);
     });
 
 const questionsJson = load(questionsUrl);
